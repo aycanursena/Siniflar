c8c2b1cfe2478f33715b1a232d12f9ac4b44df02
Ayça Nursena Korkmaz <110303195+aycanursena@users.noreply.github.com>
2022-08-24 13:09:52 +0300

    Create main.java

---
+++ Boks Oyunu/main.java
@@ -0,0 +1,10 @@
+package BoksMaci;
+
+public class main {
+    public static void main(String[] args) {
+        Boksor b1 = new Boksor("a", 10, 120, 100, 30);
+        Boksor b2 = new Boksor("b", 20, 85, 85, 40);
+        Musabaka mac = new Musabaka(b1, b2, 85, 100);
+        mac.Basla();
+    }
+}
