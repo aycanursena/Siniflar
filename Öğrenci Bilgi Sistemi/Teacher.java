40e2c3c43fd8d77af7e13295718a7f951827de30
Ayça Nursena Korkmaz <110303195+aycanursena@users.noreply.github.com>
2022-08-24 13:17:26 +0300

    Create Teacher.java

---
+++ Öğrenci Bilgi Sistemi/Teacher.java
@@ -0,0 +1,18 @@
+package OgrenciBilgiSistemi;
+
+public class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    Teacher(String name, String branch, String mpno){
+        this.name = name;
+        this.branch = branch;
+        this.mpno = mpno;
+    }
+    void prinInf(){
+        System.out.println("Öğretmen adı : " + this.name);
+        System.out.println("Öğretmenin branşı : " + this.branch);
+        System.out.println("Öğretmenin telefon numarası : " + this.mpno);
+    }
+}
