f29c21ca6925aae986bf4c3d97d52f0c624f2dcc
Ayça Nursena Korkmaz <110303195+aycanursena@users.noreply.github.com>
2022-08-24 13:17:04 +0300

    Create Student.java

---
+++ Öğrenci Bilgi Sistemi/Student.java
@@ -0,0 +1,35 @@
+package OgrenciBilgiSistemi;
+
+public class Course {
+    String name;
+    String code;
+    String prefix;
+    int note;
+    int sozlu_notu;
+    int ders_ort;
+    // mecuttaki teacher sınıfını buraya çağırma;
+    Teacher teacher;
+
+    Course(String name, String code, String prefix){
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+        this.sozlu_notu = 0;
+        this.ders_ort = 0;
+    }
+    void printTeacherInfo(){
+        this.teacher.prinInf();
+    }
+    void addTeacher(Teacher teacher){
+        if (teacher.branch.equals(this.prefix)){
+            this.teacher = teacher;
+            printTeacherInfo();
+        }else
+            System.out.println("Öğretmen ve ders uyuşmuyor!");
+    }
+    double dersOrtalamasi(){
+        this.ders_ort = ((this.note * 80 / 100) + (this.sozlu_notu * 20 / 100));
+        return this.ders_ort;
+    }
+}
