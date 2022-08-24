09691d0f42f0924d7b3432f5607ba7c247fd9064
Ayça Nursena Korkmaz <110303195+aycanursena@users.noreply.github.com>
2022-08-24 13:10:23 +0300

    Create Boksor.java

---
+++ Boks Oyunu/Boksor.java
@@ -0,0 +1,34 @@
+package BoksMaci;
+
+public class Boksor {
+    String name;
+    int damage;
+    int can;
+    int agirlik;
+    int dodge;
+    Boksor(String name, int damage, int can, int agirlik, int dodge){
+        this.name = name;
+        this.damage = damage;
+        this.can = can;
+        this.agirlik = agirlik;
+        if (dodge >= 0 && dodge <= 100){
+            this.dodge = dodge;
+        }else
+            this.dodge = 0;
+    }
+    int Vurma(Boksor rakip){
+        System.out.println(this.name + " => " + rakip.name + " " + this.damage + " " + " hasar vurdu.");
+        if (rakip.isDodge()){
+            System.out.println(rakip.name + " gelen hasarı blokladı!");
+            System.out.println("------------------------------------");
+            return rakip.can;
+        } if (rakip.can - this.damage < 0){
+            return 0;
+        }
+        return rakip.can - this.damage;
+    }
+    public boolean isDodge(){
+        double randomNumber = Math.random() * 100;
+        return randomNumber <= this.dodge;
+    }
+}
